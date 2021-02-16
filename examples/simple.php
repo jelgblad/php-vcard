3e9998ee074a1e526cba9672d95e33d68884e5d0
Jonas Elgblad <jonas.elgblad@gmail.com>
2021-02-16 11:36:37 +0100

    add simple example

---
+++ examples/simple.php
@@ -0,0 +1,16 @@
+<?php
+
+// Example autoloader; replace this with your own (or the one included in Composer)
+require '_autoload.php';
+
+use jelgblad\VCard\VCard;
+
+// Create new VCard
+$vcard = new VCard();
+
+// Add some properties to VCard
+$vcard->createProp('FN', 'Jonas Elgblad');
+$vcard->createProp('N', ['Elgblad', 'Jonas']);
+$vcard->createProp('URL', 'https://github.com/jelgblad')->createParam('TYPE', 'github');
+
+echo $vcard;
